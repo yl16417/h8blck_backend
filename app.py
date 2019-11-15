e56d28a52462c390910df8575136fc7eec593d96
yl16417 <liye@dyn3153-206.wlan.ic.ac.uk>
2019-11-15 15:27:09 +0000

    add the json

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 from flask import Flask
 from flask_restful import Api
 from request import PerspectiveHandler
+
 app = Flask(__name__)
 api = Api(app)
+
+@app.route('/analyse', method = ['GET','POST'])
+def analyse():
+	data = request.get_json()
+	handler = PerspectiveHandler()
+	handler.get(data)
+
+	return handler._html
 
 api.add_resource(PerspectiveHandler, "/analyse/<string:texts>")
 if __name__ == "__main__":
